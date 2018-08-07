824c9fa8a8156e3b490e75ae28d08e19e7afa25b
NrmThotakura <narmada.t529@gmail.com>
2018-08-07 18:37:58 +0530

    sdas

--- vuepro/src/components/Ether.vue
+++ vuepro/src/components/Ether.vue
@@ -1,35 +1,26 @@
 <template>
   <div class="hello">
     <h1>{{ price }}</h1>
-   // <h1>{{ message }}</h1>
-    <form>
-      Start Date
-      <input type="date" placeholder="Select Start Date" id="ipStartDate"/> <br>
-      End Date
-      <input type="date" placeholder="Select End Date" id="ipEndDate"/><br>
-      <button @click="clicking()">Get Message Data</button>
-    </form>
+    <h1>{{ message }}</h1>
+    <input type="text" placeholder="Message ID"/>
+    <button @click="clicking()">Change Value</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Ether",
-  data() {
-    return {
-     price: 200,
-     message: "Hello Vue!"
-     
-    };
-  },
-  methods: {
-    clicking() {
-      this.price = this.price + 100;
-      this.message = this.message.split("").reverse().join("");
-      //return price = this.StartDate + thhis.EndDate
-
-
-    }
-  }
-};
+   export default {
+   name: 'Ether',
+   data() {
+     return {
+         price : 200,
+         message: 'Hello Vue!'
+     }
+   },
+   methods: {
+      clicking() {
+         this.price = this.price  + 100;
+         this.message = this.message.split('').reverse().join('')
+      }
+   }
+}
 </script>
