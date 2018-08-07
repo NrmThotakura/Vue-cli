c31fe85472c20730ebb5feaf19e4557fe32e9735
NrmThotakura <narmada.t529@gmail.com>
2018-08-07 18:34:30 +0530

    sd

--- vuepro/src/components/Ether.vue
+++ vuepro/src/components/Ether.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ price }}</h1>
-    <h1>{{ message }}</h1>
+   // <h1>{{ message }}</h1>
     <form>
       Start Date
       <input type="date" placeholder="Select Start Date" id="ipStartDate"/> <br>
@@ -13,38 +13,20 @@
 </template>
 
 <script>
-const sql = require('mssql')
-var sqlConfig = {
-    user: 'sa',
-    password: 'sa@1234',
-    server: '192.168.1.126',  
-    database: 'SampleTest'
-  };
 export default {
   name: "Ether",
   data() {
     return {
-     // price: 200,
-     // message: "Hello Vue!"
-     StartDate: '2018-06-27',
-     EndDate: '2018-06-28'
+     price: 200,
+     message: "Hello Vue!"
+     
     };
   },
   methods: {
     clicking() {
-      //this.price = this.price + 100;
-      //this.message = this.message.split("").reverse().join("");
-      sql.connect(databaseConfig, err => {
-
-    // ... error checks
-
-if (err) {
-  console.log('error: ' + err)
-
-} else {
-  console.log('conencted')
-}
-})
+      this.price = this.price + 100;
+      this.message = this.message.split("").reverse().join("");
+      //return price = this.StartDate + thhis.EndDate
 
 
     }
