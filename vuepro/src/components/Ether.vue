f1bcb49ed5ba5f575e4b016f226327d1fd3243c8
NrmThotakura <narmada.t529@gmail.com>
2018-08-08 11:59:21 +0530

    Onclick JSON data change

--- vuepro/src/components/Ether.vue
+++ vuepro/src/components/Ether.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="hello">
-    <h1>{{ price }}</h1>
-    <h1>{{ message }}</h1>    
+    <!-- <h1>{{ price }}</h1>
+    <h1>{{ message }}</h1>     -->
+   <p>{{results[0].abstract}}</p>
     <button @click="clicking()">Change Value</button>
   </div>
 </template>
 
 <script>
-   export default {
-   name: 'Ether',
-   data() {
-     return {
-         price : 200,
-         message: 'Hello Vue!'
-     }
-   },
-   methods: {
-      clicking() {
-         this.price = this.price  + 100;
-         this.message = this.message.split('').reverse().join('')
-      }
-   }
-}
+export default {
+  name: "Ether",
+  data() {
+    return {
+      //  price : 200,
+      //  message: 'Hello Vue!'
+      results: [
+      {title: "the very first post", abstract: "first post"},
+      {title: "and then there was the second", abstract: "second post"},
+      {title: "third time's a charm", abstract: "third post"},
+      {title: "four the last time", abstract: "four post"}
+    ]
+    };
+  },
+  methods: {
+    clicking() {
+      //  this.price = this.price  + 100;
+      //  this.message = this.message.split('').reverse().join('')
+      this.results[0].abstract = this.results[1].abstract
+      
+    }
+  }
+};
 </script>
