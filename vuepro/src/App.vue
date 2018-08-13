d458c609c1c785aacad711bccf04006bd9ca76f3
NrmThotakura <narmada.t529@gmail.com>
2018-08-13 18:48:53 +0530

    sda

--- vuepro/src/App.vue
+++ vuepro/src/App.vue
@@ -5,13 +5,11 @@
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue';
 import Ether from './components/Ether.vue';
 
 export default {
   name: 'app',
   components: {
-    //HelloWorld,
     Ether
   }
 }
