e93cee2f22ef68cdf46ae5620c20756d550ee74d
NrmThotakura <narmada.t529@gmail.com>
2018-08-13 12:59:40 +0530

    ssd

--- vuepro/src/start.js
+++ vuepro/src/start.js
@@ -1,43 +1,42 @@
-var http = require("http");
-var express = require('express');
-var app = express();
-var mysql      = require('mssql');
-var bodyParser = require('body-parser');
-
-var connection = mysql.createConnection({
+var sqlInstance = require("mssql");
+var setUp = {
   server: "192.168.1.126",
   database: "SampleTest",
   user: "sa",
   password: "sa@1234"
-});
- 
-connection.connect(function(err) {
-  if (err) throw err
-  console.log('You are now connected...')
-})
-
- 
-
-app.use( bodyParser.json() );      
-app.use(bodyParser.urlencoded({    
-  extended: true
-})); 
-
-var server = app.listen(8080,  "192.168.1.205", function () {
- 
-  var host = server.address().address
-  var port = server.address().port
- 
-  console.log("Example app listening at http://%s:%s", host, port)
- 
-});
- 
-app.get('/', function (req, res) {
-   connection.query('select * from UnitType', function (error, results, fields) {
-   if (error) throw error;
-   res.end(JSON.stringify(results));
- });
-});
+};
+function executeDatabaseActions(dbData) {
+  sqlInstance.connect(setUp).then(function() {
+    new sqlInstance.Request()
+      // .query("CALL [Getmessage_Narmada]('2018-07-17', '2018-07-20')")
+      .query("SELECT * FROM UnitType")
+      .then(function(dbData) {
+        if (dbData == null || dbData.length === 0)
+          $.ajax({
+            dataType: "json",
+            type: "POST",
+            contentType: "application/json;charset=utf-8",
+            url: "http://localhost:8080/",
+            data: JSON.stringify(dbData),
+            success: function(dbData) {
+              console.log(dbData);
+              if (dbData.length > 0) {
+              //return  alert(dbData[0].name)
+              res.end(JSON.stringify(results));
+              } else {
+                // if (response.errno) {
+                //   self.errLoginMsg("Server Error: " + response.errno);
+                // } else self.alert("No Data");
+                alert("No Data");
+              }
+            },
+            // error: function(d) {
+            //   console.log(d);
+            // }
+          });
+      });
+  });
+}
 
 // const app = require('../server')
 
