3505a3c10f68507eb61f18f54ca047d31cfa8757
NrmThotakura <narmada.t529@gmail.com>
2018-08-14 11:59:30 +0530

    getDB data

--- server/app.js
+++ server/app.js
@@ -1,44 +1,31 @@
-var http = require("http");
 var express = require('express');
 var app = express();
-var mysql      = require('mysql');
-var bodyParser = require('body-parser');
- 
-//start mysql connection
-var connection = mysql.createConnection({
-  server: "192.168.1.126",
-  database: "SampleTest",
-  user: "sa",
-  password: "sa@1234"
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+    var config = {
+        user: 'sa',
+        password: 'sa@1234',
+        server: '192.168.1.126', 
+        database: 'SampleTest' 
+    };
+    new sql.ConnectionPool(config).connect().then(pool => {
+      return pool.request().query("select * from Student")
+      }).then(result => {
+        let rows = result.recordset
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.status(200).json(rows);
+        sql.close();
+      }).catch(err => {
+        res.status(500).send({ message: "${err}"})
+        sql.close();
+      });
 });
- 
-connection.connect(function(err) {
-  if (err) throw err
-  console.log('You are now connected...')
-})
-//end mysql connection
- 
-//start body-parser configuration
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-//end body-parser configuration
- 
-//create app server
-var server = app.listen(8080,  "192.168.1.205", function () {
- 
-  var host = server.address().address
-  var port = server.address().port
- 
-  console.log("Example app listening at http://%s:%s", host, port)
- 
-});
- 
-//rest api to get all results
-app.get('/UnitType', function (req, res) {
-   connection.query('select * from UnitType', function (error, results, fields) {
-   if (error) throw error;
-   res.end(JSON.stringify(results));
- });
+
+
+var server = app.listen(8080, function () {
+    console.log('Server is running..');
 });
