ad65f09ecbea71eb4554e81627a97279bf335a75
NrmThotakura <narmada.t529@gmail.com>
2018-08-16 18:34:56 +0530

    sd

--- vuepro/src/components/LoginForm.vue
+++ vuepro/src/components/LoginForm.vue
@@ -7,29 +7,33 @@
     </div>
 </template>
 <script>
-    export default {
-        name: 'Login',
-        data() {
-            return {
-                input: {
-                    username: "",
-                    password: ""
-                }
-            }
-        },
-        methods: {
-            login() {
-                if(this.input.username != "" && this.input.password != "") {
-                    if(this.input.username == this.$parent.testAccount.username && this.input.password == this.$parent.testAccount.password) {
-                       this.authenticated = true
-                        this.$router.push({ path: "success" });
-                    } else {
-                        alert("The username and / or password is incorrect");
-                    }
-                } else {
-                    alert("A username and password must be present");
-                }
-            }
+export default {
+  name: "Login",
+  data() {
+    return {
+      input: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    login() {
+      if (this.input.username != "" && this.input.password != "") {
+        if (
+          this.input.username == this.$parent.testAccount.username &&
+          this.input.password == this.$parent.testAccount.password
+        ) {
+          this.authenticated = true;
+          this.$router.push({ path: "success" });
+        } else {
+           alert("The username and / or password is incorrect");
         }
+      } else {
+        alert("A username and password must be present");
+
+      }
     }
+  }
+};
 </script>
